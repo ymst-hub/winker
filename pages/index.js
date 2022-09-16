d4781b41aa4a7a57f8d7500d3a7f3fdb5e34e65d
Yu <eventbaito@yahoo.co.jp>
2022-09-16 12:05:14 +0900

    コミット

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
 
-const props1 = { name: "Twitter", url: "https://twitter.com/?lang=ja"}
+import Image from 'next/image'
+import styles from '../styles/index.module.css'
+
+const props1 = { name: "Twitter", url: "https://twitter.com/?lang=ja" }
 const props2 = { name: "LINE", url: "https://line.me/ja/" }
-const props3 = { name: "Github", url: "https://github.com/"}
+const props3 = { name: "Github", url: "https://github.com/" }
 
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <div>Winker</div>
-      </Head>
+      <header className={styles.header}>
+        <h1 className={styles.logo}>Winker</h1>
+      </header>
+    
       <main>
-        <Button {...props1} />
-        <Button {...props2} />
-        <Button {...props3} />
+        <ul>
+          <Button {...props1} />
+          <Button {...props2} />
+          <Button {...props3} />
+        </ul>
       </main>
     </div>
   )
 }
 
-function Button({ name, url }){
+function Button({ name, url }) {
   return (
-    <button>
-      <a href={url}>
-        <p>{name}</p>
-      </a>
-    </button>
+    <li>
+      <button>
+        <a href={url}>
+          {name}
+        </a>
+      </button>
+    </li>
   )
 }
